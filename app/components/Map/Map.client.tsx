4b897f006ffcc6db7dc3b103aa27f008a6b8d57d
Garry T <incarnate@rogers.com>
2024-06-12 23:29:24 -0400

    Added Polylines to indicate the different GO Transit Rail Lines.

--- app/components/Map/Map.client.tsx
+++ app/components/Map/Map.client.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Polyline, TileLayer } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import type { LatLngBoundsLiteral, Map as MapType } from "leaflet";
 import { TrainTrip } from "types";
@@ -8,6 +8,13 @@ import Interactions from "../Interactions";
 import Markers from "../Markers/Markers.client";
 
 import "leaflet/dist/leaflet.css";
+
+import { BALA_LATLONS } from "~/railSubdivisions/bala";
+import { KINGSTON_LATLONS } from "~/railSubdivisions/kingston";
+import { NEWMARKET_LATLONS } from "~/railSubdivisions/newmarket";
+import { OAKVILLE_LATLONS } from "~/railSubdivisions/oakville";
+import { UXBRIDGE_LATLONS } from "~/railSubdivisions/uxbridge";
+import { WESTON_LATLONS } from "~/railSubdivisions/weston";
 
 type MapProps = {
   //   trips: Trip[];
@@ -46,6 +53,24 @@ export default function Map({ trips }: MapProps) {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Polyline
+          pathOptions={{ color: "green" }}
+          positions={KINGSTON_LATLONS}
+        />
+        <Polyline
+          pathOptions={{ color: "purple" }}
+          positions={UXBRIDGE_LATLONS}
+        />
+        <Polyline pathOptions={{ color: "orange" }} positions={BALA_LATLONS} />
+        <Polyline
+          pathOptions={{ color: "blue" }}
+          positions={NEWMARKET_LATLONS}
+        />
+        <Polyline pathOptions={{ color: "brown" }} positions={WESTON_LATLONS} />
+        <Polyline
+          pathOptions={{ color: "yellow" }}
+          positions={OAKVILLE_LATLONS}
         />
         <MarkerClusterGroup
           chunkedLoading
